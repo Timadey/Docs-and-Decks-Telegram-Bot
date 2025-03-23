9ccf4ff4e66cb98e631d6c0ba217a5276637e514
Timothy Adeleke <65812744+Timadey@users.noreply.github.com>
2025-03-23 09:32:17 +0100

    use score sheet instead of worksheet to get cell value

--- repository.py
+++ repository.py
@@ -50,7 +50,7 @@ class Repository:
 
             # Create a dictionary mapping headers to values
             scores_dict = {headers[i]: scores[i] if i < len(scores) else "N/A" for i in range(len(headers))}
-            total_score = self.gsheet.worksheet.acell("O1")
+            total_score = self.score_sheet.acell("O1")
 
             return {
                 "Full Name": scores_dict.get("Full Name", "N/A"),
